daf0c4dfb92797e8d99bf11d124869da58ef84b6
Rohit Yadav <ydrohit182@gmail.com>
2025-06-09 16:55:24 +0530

    Update Chat.js

--- src/componats/Chat.js
+++ src/componats/Chat.js
@@ -9,7 +9,7 @@ const Chat = () => {
 
     // Establish a connection to the Socket.IO server
 // Replace 'http://localhost:3000' with your server URL if different
-    const socket = useMemo(() => io('http://localhost:3000', {
+    const socket = useMemo(() => io('https://chatbot-server2.onrender.com', {
   withCredentials: true,
   transports: ["websocket"], // Bypass CORS for polling
   reconnectionAttempts: 3,
